c998b7794f6454df13d5d633c058d7051d04ec7b
OzoOnne <55187541+OzoOnne@users.noreply.github.com>
2026-04-17 08:50:51 +0200

    Edit.cshtml toegevoegd -> form voor het editen van een festival

---
+++ Showcase-project/Showcase-WebApp/Pages/Festivals/Edit.cshtml.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Showcase_WebApp.Models;
+
+namespace Showcase_WebApp.Pages.Festivals
+{
+    public class EditModel : PageModel
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        [BindProperty]
+        public FestivalDto Festival { get; set; } = new();
+
+        public EditModel(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient("FestivalApi");
+            var festival = await client.GetFromJsonAsync<FestivalDto>($"api/festival/{id}");
+            if (festival == null)
+            {
+                return NotFound();
+            }
+
+            Festival = festival;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var client = _httpClientFactory.CreateClient("FestivalApi");
+
+            Festival.Date = DateTime.SpecifyKind(Festival.Date, DateTimeKind.Utc);
+
+            var response = await client.PutAsJsonAsync($"api/festivals/{Festival.Id}", Festival);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                ModelState.AddModelError(string.Empty, "Er ging iets mis bij het updaten van het festival.");
+                return Page();
+            }
+
+            return RedirectToPage("/Festivals/Index");
+        }
+    }
+    }
+}
